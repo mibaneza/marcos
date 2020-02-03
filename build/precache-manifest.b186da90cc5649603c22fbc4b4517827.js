821c687e7f0caf0db585c0685e4a8cd32d7742ee
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-02-02 22:08:52 -0500

    agua
    gaaaa

---
+++ build/precache-manifest.b186da90cc5649603c22fbc4b4517827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1441f888aafe7385c88e70e77b48741b",
+    "url": "/marcos/index.html"
+  },
+  {
+    "revision": "46360a63d5bb7b533049",
+    "url": "/marcos/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "765b7d3f17a8d622975f",
+    "url": "/marcos/static/js/2.73b41756.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/marcos/static/js/2.73b41756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46360a63d5bb7b533049",
+    "url": "/marcos/static/js/main.f071ffeb.chunk.js"
+  },
+  {
+    "revision": "41cc594351d9a745e614",
+    "url": "/marcos/static/js/runtime-main.a60fcc44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marcos/static/media/logo.5d5d9eef.svg"
+  }
+]);
